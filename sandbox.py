5ec7a954658e48d25fbcc37175f41e8d304e2f6e
vuonga1103 <vuonga1103@yahoo.com>
2021-01-01 21:01:59 -0500

    decorator

--- sandbox.py
+++ sandbox.py
@@ -1,16 +1,14 @@
-try:
-    # opens a file to write, if that file doesn't exist, create it
-    f = open('testfile', 'r')
-    f.write('Write a test line')
-# Can specify in except specific error you want to accept for
-except TypeError:
-    print('There was a type error')
-except OSError:
-    # Error that occurs when you open and writing to files you don't have permission to
-    print('Hey you have an OS error')
-finally:
-    print('I always run')
+def my_decorator(og_function):
+    def wrap_fn():
+        print('a bow')
+        og_function()
+        print('some tape')
+    return wrap_fn
 
-# (base) anhvuong@Anhs-Air python-learning % python3 sandbox.py
-# Hey you have an OS error
-# I always run
+
+@my_decorator
+def say_hello():
+    print('hello!!!')
+
+
+say_hello()
