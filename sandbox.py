96e932f358624ffa68fdde94673213c7b5cfb4d2
vuonga1103 <vuonga1103@yahoo.com>
2021-01-02 10:15:26 -0500

    example scraping for book titles on multiple pages with 2 star rating

--- sandbox.py
+++ sandbox.py
@@ -1,24 +1,39 @@
 import requests
 import bs4
 
-res = requests.get('https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)')
-soup = bs4.BeautifulSoup(res.text, 'lxml')
+# base_url = "http://books.toscrape.com/catalogue/page-{}.html"
 
-print(soup.select('.thumbimage'))
-# prints all images with class thumbimage
+# res = requests.get(base_url.format(1))
+# soup = bs4.BeautifulSoup(res.text, 'lxml')
 
-computer = soup.select('.thumbimage')[0]
-print(computer)
-# <img alt="" class="thumbimage" data-file-height="600" data-file-width="800" decoding="async" height="165" src="//upload....
+# products = soup.select('.product_pod')
 
-# making request for the image
-image_link = requests.get('https:' + computer['src'])
-image_link.content  # binary file for image that can be saved to computer
+# example = products[0]
 
-# pass into open() file name you want to call the image, mode: wb (write binary)
-f = open('my_computer_image.jpg', 'wb')
-# write to the file
-f.write(image_link.content)
-f.close()
+# # print('star-rating Two' in str(example))
+# # => False, quick and dirty way to check if rating is 3 star
 
-# the above saves the image to current working directory. but to save it somewhere else you can specify the full path, ex. C://Users...
+# # better way...
+# print(example.select('.star-rating.Two'))
+# # will return a list that has selection or an empty list
+# # [] == example.select('.star-rating.Two')
+
+# print(example.select('a')[1]['title']) # A Light in the Attic
+
+base_url = "http://books.toscrape.com/catalogue/page-{}.html"
+
+book_titles = []
+
+for page_num in range(0, 51):
+    res = requests.get(base_url.format(page_num))
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    products = soup.select('.product_pod')
+
+    for product in products:
+        has_two_star_rating = ([] == product.select('.star-rating.Two'))
+
+        if has_two_star_rating:
+            title = product.select('a')[1]['title']
+            book_titles.append(title)
+
+print(book_titles)
